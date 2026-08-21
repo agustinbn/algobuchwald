1efb6929df029febdc77819135ba4cb22ed4bc03
Agustín Bianchi <bianchiagus6@gmail.com>
2026-08-21 00:00:29 -0300

    feat(tp0): implementar Swap, Maximo, Seleccion y Suma

--- tp0/ejercicios/tp0.go
+++ tp0/ejercicios/tp0.go
@@ -2,13 +2,19 @@ package ejercicios
 
 // Swap intercambia dos valores enteros.
 func Swap(x *int, y *int) {
-
+	*x, *y = *y, *x
 }
 
 // Maximo devuelve la posición del mayor elemento del arreglo, o -1 si el el arreglo es de largo 0. Si el máximo
 // elemento aparece más de una vez, se debe devolver la primera posición en que ocurre.
 func Maximo(vector []int) int {
-	return -1
+	posicion_maximo := -1
+	for k, v := range vector {
+		if posicion_maximo == -1 || v > vector[posicion_maximo] {
+			posicion_maximo = k
+		}
+	}
+	return posicion_maximo
 }
 
 // Comparar compara dos arreglos de longitud especificada.
@@ -21,14 +27,29 @@ func Comparar(vector1 []int, vector2 []int) int {
 
 // Seleccion ordena el arreglo recibido mediante el algoritmo de selección.
 func Seleccion(vector []int) {
+	for i := 0; i < len(vector)-1; i++ {
+		posicion_minimo := i
+		for j := i + 1; j < len(vector); j++ {
+			if vector[j] < vector[posicion_minimo] {
+				posicion_minimo = j
+			}
+		}
+		vector[i], vector[posicion_minimo] = vector[posicion_minimo], vector[i]
+	}
+}
 
+func sumarValores(vector []int, pos int, acumulado int) int {
+	if pos == len(vector) {
+		return acumulado
+	}
+	return sumarValores(vector, pos+1, acumulado+vector[pos])
 }
 
 // Suma devuelve la suma de los elementos de un arreglo. En caso de no tener elementos, debe devolver 0.
 // Esta función debe implementarse de forma RECURSIVA. Se puede usar una función auxiliar (que sea
 // la recursiva).
 func Suma(vector []int) int {
-	return 0
+	return sumarValores(vector, 0, 0)
 }
 
 // EsCadenaCapicua devuelve si la cadena es un palíndromo. Es decir, si se lee igual al derecho que al revés.
