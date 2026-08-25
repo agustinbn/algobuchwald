f550ea718b9ec35283fa400d6cab95e883252b45
Agustín Bianchi <bianchiagus6@gmail.com>
2026-08-24 21:34:05 -0300

    feat(tp0): leer archivos de entrada y ordenar el vector menor

--- tp0/main.go
+++ tp0/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"tp0/ejercicios"
+)
+
+func procesar_archivo(ruta string, vector *[]int) {
+	archivo, err := os.Open(ruta)
+    if err != nil {
+      fmt.Printf("Error %v al abrir el archivo %s", ruta, err)
+      return
+    }
+    defer archivo.Close()
+
+    s := bufio.NewScanner(archivo)
+    i := 0
+    for s.Scan() {
+    	var ts int
+    	ts, err = strconv.Atoi(s.Text())
+     	*vector = append(*vector, ts)
+    	i++
+    }
+    err = s.Err()
+    if err != nil {
+        fmt.Println(err)
+    }
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	vector_1 := []int{}
+	vector_2 := []int{}
+	procesar_archivo("archivo1.in", &vector_1)
+	procesar_archivo("archivo2.in", &vector_2)
+	if ejercicios.Comparar(vector_1, vector_2) == 1 {
+		ejercicios.Seleccion(vector_1)
+		for i := range vector_1 {
+			fmt.Println(vector_1[i])
+		}
+	} else {
+		ejercicios.Seleccion(vector_2)
+		for i := range vector_2 {
+			fmt.Println(vector_2[i])
+		}
+	}
 }
